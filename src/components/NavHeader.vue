cb2cbefdd2a200c9f7f314155370c9da18054f6b
xubin <1136053514@qq.com>
2020-11-15 21:54:28 +0800

    头部组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -22,7 +22,29 @@
     </div>
     <!-- 商品菜单 -->
     <div class="top_header">
-      <div class="container">caidan</div>
+      <div class="container">
+        <div class="logo">
+          <a href="/#/home"></a>
+        </div>
+        <div class="menu">
+          <div class="item">
+            <span>小米手机</span>
+            <div class="children">
+              <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="search">
+          <input class="input" type="text" placeholder="小米10pro" />
+          <input type="button" class="span">
+          <!-- <span></span> -->
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -68,6 +90,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "../assets/scss/config.scss";
+@import "../assets/scss/base.scss";
 a {
   display: inline-block;
   margin-right: 17px;
@@ -88,17 +111,17 @@ a {
       display: flex;
       justify-content: space-between;
       align-items: center;
-      margin: 0 auto;
-      width: 1226px;
       .user {
         display: flex;
         align-items: center;
         .cart {
+          margin-right: 0;
           display: flex;
           justify-content: center;
           align-items: center;
           width: 110px;
           height: 40px;
+          background-color: #424242;
           &:hover {
             color: white;
             background: $colorA;
@@ -110,6 +133,104 @@ a {
             height: 12px;
             background: url("/imgs/icon-cart-checked.png") no-repeat center;
             background-size: contain;
+          }
+        }
+      }
+    }
+  }
+  .top_header {
+    margin: 20px 0;
+    height: 112px;
+    .container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .logo {
+        width: 55px;
+        height: 55px;
+        background-color: $colorA;
+        a {
+          width: 110px;
+          height: 55px;
+          margin-left: 0px;
+          transition: 0.5s;
+          &:hover {
+            margin-left: -55px;
+            transition: 0.5s;
+          }
+          &::before {
+            content: "";
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url("/imgs/mi-logo.png") no-repeat center;
+            background-size: 55px;
+          }
+          &::after {
+            content: "";
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url("/imgs/mi-home.png") no-repeat center;
+            background-size: 55px;
+          }
+        }
+      }
+      .menu {
+        margin-left: 210px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        flex: 1;
+        .item {
+          span {
+            margin-right: 20px;
+            font-size: 16px;
+            font-weight: bold;
+            color: #333333;
+            cursor: pointer;
+          }
+          &:hover {
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            position: relative;
+            display: none;
+            ul {
+              display: flex;
+              li {
+                color: red;
+              }
+            }
+          }
+        }
+      }
+      .search {
+        display: flex;
+        width: 320px;
+        border: 1px solid #e0e0e0;
+        .input {
+          width: 245px;
+          height: 55px;
+          padding: 0 10px;
+          font-size: 14px;
+          border: none;
+        }
+        .span {
+          display: flex;
+          flex: 1;
+          justify-content: center;
+          align-items: center;
+          padding: 4px 10px;
+          border:none;
+          border-left: 1px solid #e0e0e0;
+          background: url('/imgs/icon-search.png') no-repeat center;
+          background-size: 20px;
+          &:hover{
+            background-color: $colorA;
+            color: #fff;
           }
         }
       }
