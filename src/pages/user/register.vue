7d22094d85aef3f97bd470b8da70ce267c98e2a9
徐彬 <1136053514@qq.com>
2020-11-18 16:43:23 +0800

    删除

--- src/pages/user/register.vue
+++ src/pages/user/register.vue
@@ -1,34 +1,20 @@
 <template>
     <div>
         注册界面
-
-        <div :class="{active: isActive}" @click=" isActive= !isActive">动态绑定</div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            userInfo: {
-                username: '',
-                password: '',
-                email: ''
-            },
-            isActive: true
         }
     },
     methods: {
         async register() {
             let res = await this.axios.post('/user/register',{...this.userInfo})
         },
-        handleClick(){
-            this.isActive = !this.isActive
-        }
     }
 }
 </script>
 <style lang="scss" scoped>
-.active{
-    color: red;
-}
 </style>
