dee01b3c90cbb030e206d986facee8b520f5eb38
徐彬 <1136053514@qq.com>
2020-11-24 15:25:39 +0800

    购物车Fun

--- src/components/AddCart.vue
+++ src/components/AddCart.vue
@@ -40,8 +40,7 @@ export default {
           this.flag = !this.flag
       },
       goToCart(){
-          // this.$emit('goToCart')
-          this.$router.goNotFound()
+          this.$emit('goToCart')
       }
   },
 };
